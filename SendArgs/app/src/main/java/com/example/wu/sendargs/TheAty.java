166f85eec3fe7f59e4aceb95cecf36165587ca43
“gao <wu120051315@163.com>
2020-02-09 15:22:06 +0800

    Activity passes a string to another Activity.

---
+++ SendArgs/app/src/main/java/com/example/wu/sendargs/TheAty.java
@@ -0,0 +1,45 @@
+package com.example.wu.sendargs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TheAty extends AppCompatActivity {
+
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_aty);
+
+        Intent i = getIntent();
+
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText(i.getStringExtra("data"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_the_aty, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
