f19d8b8741ed85b82e82eae5fa5f44b929e8c711
“gao <wu120051315@163.com>
2020-02-10 18:45:59 +0800

    Get the return parameters of the activity.

--- SendArgs/app/src/main/java/com/example/wu/sendargs/MainActivity.java
+++ SendArgs/app/src/main/java/com/example/wu/sendargs/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
 
         findViewById(R.id.btnStartAty).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent i =new Intent(MainActivity.this,TheAty.class);
 
                 i.putExtra("user",new User("gaoquan.wu",26));
-                startActivity(i);
+//                startActivity(i);
 
+                startActivityForResult(i,0);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        textView.setText("另一个Activity返回的数据是："+data.getStringExtra("data"));
     }
 
     @Override
--- SendArgs/app/src/main/java/com/example/wu/sendargs/TheAty.java
+++ SendArgs/app/src/main/java/com/example/wu/sendargs/TheAty.java
@@ -5,21 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TheAty extends AppCompatActivity {
 
     private TextView tv;
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_aty);
         tv = (TextView) findViewById(R.id.tv);
+        editText = (EditText) findViewById(R.id.editText);
+
         Intent i = getIntent();
 
         User user = i.getParcelableExtra("user");
         tv.setText(String.format("User info(name=%s,age=%d)",user.getName(),user.getAge()));
 
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.putExtra("data",editText.getText().toString());
+                setResult(1, i);
+                finish();
+            }
+        });
     }
 
     @Override
